d9656982548578d4eb7cae5ef4b6f612fc01168b
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-03 21:13:50 +0200

    Fix layout and styling issues

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,9 +7,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-background-light dark:bg-background-dark">
+    <div className="min-h-screen flex flex-col bg-background-light dark:bg-background-dark transition-colors duration-200">
       <HeaderNavigation />
-      <main className="flex-1 container mx-auto px-4 py-8 mt-16">
+      <main className="flex-1 container mx-auto px-4 py-8 mt-20">
         {children}
       </main>
     </div>
