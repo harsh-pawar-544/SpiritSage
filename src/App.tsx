07b3cf193817a68faeb691038ff53b172bb476b1
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-03 15:15:06 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import SpiritOverviewPage from './pages/SpiritOverview/SpiritOverviewPage';
+import CategoryPage from './pages/CategoryPage';
 import SpiritPage from './pages/SpiritPage';
 import { SpiritsProvider } from './contexts/SpiritsContext';
-import { UserPreferencesProvider } from './contexts/UserPreferencesContext';
-import { RecommendationsProvider } from './contexts/RecommendationsContext';
 
 function App() {
   return (
-    <UserPreferencesProvider>
-      <SpiritsProvider>
-        <RecommendationsProvider>
-          <Router>
-            <Toaster position="top-center" />
-            <Layout>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/category/:id" element={<SpiritOverviewPage />} />
-                <Route path="/spirit/:id" element={<SpiritPage />} />
-              </Routes>
-            </Layout>
-          </Router>
-        </RecommendationsProvider>
-      </SpiritsProvider>
-    </UserPreferencesProvider>
+    <SpiritsProvider>
+      <Router>
+        <Toaster position="top-center" />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/category/:id" element={<CategoryPage />} />
+            <Route path="/spirit/:id" element={<SpiritPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </SpiritsProvider>
   );
 }
 
