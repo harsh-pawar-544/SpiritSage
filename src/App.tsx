98f2326f394afab5865f08389617d5f5ae23f520
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-03 16:52:27 +0200

    Add RecommendationsProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { SpiritsProvider } from './contexts/SpiritsContext';
 import { UserPreferencesProvider } from './contexts/UserPreferencesContext';
+import { RecommendationsProvider } from './contexts/RecommendationsContext';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import SpiritListPage from './pages/SpiritList/SpiritListPage';
@@ -17,20 +18,22 @@ function App() {
   return (
     <UserPreferencesProvider>
       <SpiritsProvider>
-        <Router>
-          <Toaster position="top-center" />
-          <Layout>
-            <Routes>
-              <Route path="/" element={<SpiritListPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
-              <Route path="/category/:id" element={<SpiritOverviewPage />} />
-              <Route path="/category/:id/subtypes" element={<SpiritSubtypesPage />} />
-              <Route path="/spirit/:id" element={<SpiritProfilePage />} />
-            </Routes>
-          </Layout>
-        </Router>
+        <RecommendationsProvider>
+          <Router>
+            <Toaster position="top-center" />
+            <Layout>
+              <Routes>
+                <Route path="/" element={<SpiritListPage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/category/:id" element={<SpiritOverviewPage />} />
+                <Route path="/category/:id/subtypes" element={<SpiritSubtypesPage />} />
+                <Route path="/spirit/:id" element={<SpiritProfilePage />} />
+              </Routes>
+            </Layout>
+          </Router>
+        </RecommendationsProvider>
       </SpiritsProvider>
     </UserPreferencesProvider>
   );
