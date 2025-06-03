56d6e89076efe7c2074bdc7b67c3baa2f51b5925
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-03 15:17:22 +0200

    Add RecommendationsProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,24 @@ import SpiritOverviewPage from './pages/SpiritOverview/SpiritOverviewPage';
 import SpiritProfilePage from './pages/Spirit/[id]';
 import { SpiritsProvider } from './contexts/SpiritsContext';
 import { SpiritSageProvider } from './contexts/SpiritSageContext';
+import { RecommendationsProvider } from './contexts/RecommendationsContext';
 
 function App() {
   return (
     <SpiritSageProvider>
       <SpiritsProvider>
-        <Router>
-          <Toaster position="top-center" />
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/category/:id" element={<SpiritOverviewPage />} />
-              <Route path="/spirit/:id" element={<SpiritProfilePage />} />
-            </Routes>
-          </Layout>
-        </Router>
+        <RecommendationsProvider>
+          <Router>
+            <Toaster position="top-center" />
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/category/:id" element={<SpiritOverviewPage />} />
+                <Route path="/spirit/:id" element={<SpiritProfilePage />} />
+              </Routes>
+            </Layout>
+          </Router>
+        </RecommendationsProvider>
       </SpiritsProvider>
     </SpiritSageProvider>
   );
