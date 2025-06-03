e4173d18164d8f4f365c76d51d6b0a5116dad515
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-03 15:19:02 +0200

    Update App structure and providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import SpiritOverviewPage from './pages/SpiritOverview/SpiritOverviewPage';
-import SpiritProfilePage from './pages/Spirit/[id]';
 import { SpiritsProvider } from './contexts/SpiritsContext';
 import { SpiritSageProvider } from './contexts/SpiritSageContext';
 import { UserPreferencesProvider } from './contexts/UserPreferencesContext';
 import { RecommendationsProvider } from './contexts/RecommendationsContext';
+import { UserProfileProvider } from './contexts/UserProfileContext';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
+import SpiritListPage from './pages/SpiritList/SpiritListPage';
+import SpiritOverviewPage from './pages/SpiritOverview/SpiritOverviewPage';
+import SpiritSubtypesPage from './pages/SpiritSubtypes/SpiritSubtypesPage';
+import SpiritProfilePage from './pages/Spirit/[id]';
+import AboutPage from './pages/About/AboutPage';
+import ContactPage from './pages/Contact/ContactPage';
+import SettingsPage from './pages/Settings/SettingsPage';
 
 function App() {
   return (
-    <SpiritSageProvider>
+    <UserProfileProvider>
       <UserPreferencesProvider>
-        <SpiritsProvider>
-          <RecommendationsProvider>
-            <Router>
-              <Toaster position="top-center" />
-              <Layout>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/category/:id" element={<SpiritOverviewPage />} />
-                  <Route path="/spirit/:id" element={<SpiritProfilePage />} />
-                </Routes>
-              </Layout>
-            </Router>
-          </RecommendationsProvider>
-        </SpiritsProvider>
+        <SpiritSageProvider>
+          <SpiritsProvider>
+            <RecommendationsProvider>
+              <Router>
+                <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+                <Layout>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/explore" element={<SpiritListPage />} />
+                    <Route path="/category/:id" element={<SpiritOverviewPage />} />
+                    <Route path="/category/:id/subtypes" element={<SpiritSubtypesPage />} />
+                    <Route path="/spirit/:id" element={<SpiritProfilePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path="/settings" element={<SettingsPage />} />
+                  </Routes>
+                </Layout>
+              </Router>
+            </RecommendationsProvider>
+          </SpiritsProvider>
+        </SpiritSageProvider>
       </UserPreferencesProvider>
-    </SpiritSageProvider>
+    </UserProfileProvider>
   );
 }
 
