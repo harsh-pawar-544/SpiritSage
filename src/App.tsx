25cb700dcd019a145c42dc36cf2c330a5a4dc0fd
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-03 15:16:20 +0200

    Add SpiritSageProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,24 @@ import Home from './pages/Home';
 import SpiritOverviewPage from './pages/SpiritOverview/SpiritOverviewPage';
 import SpiritProfilePage from './pages/Spirit/[id]';
 import { SpiritsProvider } from './contexts/SpiritsContext';
+import { SpiritSageProvider } from './contexts/SpiritSageContext';
 
 function App() {
   return (
-    <SpiritsProvider>
-      <Router>
-        <Toaster position="top-center" />
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/category/:id" element={<SpiritOverviewPage />} />
-            <Route path="/spirit/:id" element={<SpiritProfilePage />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </SpiritsProvider>
+    <SpiritSageProvider>
+      <SpiritsProvider>
+        <Router>
+          <Toaster position="top-center" />
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/category/:id" element={<SpiritOverviewPage />} />
+              <Route path="/spirit/:id" element={<SpiritProfilePage />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </SpiritsProvider>
+    </SpiritSageProvider>
   );
 }
 
