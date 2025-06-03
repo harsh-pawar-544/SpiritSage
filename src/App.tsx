89fffa026414969653da1b9448079bacf82c228d
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-03 15:13:20 +0200

    Add RecommendationsProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,24 @@ import SpiritOverviewPage from './pages/SpiritOverview/SpiritOverviewPage';
 import SpiritPage from './pages/SpiritPage';
 import { SpiritsProvider } from './contexts/SpiritsContext';
 import { UserPreferencesProvider } from './contexts/UserPreferencesContext';
+import { RecommendationsProvider } from './contexts/RecommendationsContext';
 
 function App() {
   return (
     <UserPreferencesProvider>
       <SpiritsProvider>
-        <Router>
-          <Toaster position="top-center" />
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/category/:id" element={<SpiritOverviewPage />} />
-              <Route path="/spirit/:id" element={<SpiritPage />} />
-            </Routes>
-          </Layout>
-        </Router>
+        <RecommendationsProvider>
+          <Router>
+            <Toaster position="top-center" />
+            <Layout>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/category/:id" element={<SpiritOverviewPage />} />
+                <Route path="/spirit/:id" element={<SpiritPage />} />
+              </Routes>
+            </Layout>
+          </Router>
+        </RecommendationsProvider>
       </SpiritsProvider>
     </UserPreferencesProvider>
   );
