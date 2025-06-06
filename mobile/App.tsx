32587b9a2f3dfae44a8b8ecd4a3fc68b9428fdbd
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-06 20:07:40 +0200

    Updated App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -78,7 +78,7 @@ function AppNavigator() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {isAuthenticated ? (
           <>
-            <Stack.Screen name="Main" component={TabNavigator} />
+            <Stack.Screen name="Main\" component={TabNavigator} />
             <Stack.Screen 
               name="AlcoholType" 
               component={AlcoholTypeScreen}
