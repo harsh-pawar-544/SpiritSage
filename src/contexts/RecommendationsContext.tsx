acfcd03609121ab4e10093703509a0e411aeb801
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-08 12:50:19 +0200

    Updated RecommendationsContext.tsx

--- src/contexts/RecommendationsContext.tsx
+++ src/contexts/RecommendationsContext.tsx
@@ -314,6 +314,7 @@ export const RecommendationsProvider: React.FC<{ children: React.ReactNode }> = 
         clearInteractionHistory,
         isLoading
       }}
+    
     >
       {children}
     </RecommendationsContext.Provider>
