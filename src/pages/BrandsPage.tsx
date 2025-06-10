607d0d65c3dfa41b799f9a3dd26977da5417e2f4
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-10 16:40:48 +0200

    Updated BrandsPage.tsx

---
+++ src/pages/BrandsPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BrandsPage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-6">Explore Brands</h1>
+      <p className="text-gray-700 dark:text-gray-300">
+        This is where we will display information about different spirit brands and their example bottles.
+      </p>
+      {/* We'll add content here */}
+    </div>
+  );
+};
+
+export default BrandsPage;
