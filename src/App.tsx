9255659afb9feba9105f2ded3317d1db414b0f30
Harsh Pawar <63223919+harsh-pawar-544@users.noreply.github.com>
2025-06-11 00:46:46 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,63 @@
-// Inside src/App.tsx
-
+// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
+import HomePage from './pages/HomePage';
+import ExplorePage from './pages/ExplorePage';
+import SpiritProfilePage from './pages/Spirit/[id]'; // Your spirit detail page
+import SpiritOverviewPage from './pages/AlcoholType/[id]'; // Alcohol type detail page
+import SubtypeDetailPage from './pages/Subtype/[id]'; // Subtype detail page
+import MyBarPage from './pages/MyBarPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import SettingsPage from './pages/SettingsPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 import { AuthProvider } from './contexts/AuthContext';
 import { SpiritsProvider } from './contexts/SpiritsContext';
-import { UserPreferencesProvider } from './contexts/UserPreferencesContext';
 import { RecommendationsProvider } from './contexts/RecommendationsContext';
-import Layout from './components/layout/Layout';
-import Home from './pages/Home';
-import SpiritListPage from './pages/SpiritList/SpiritListPage';
-import MyBarPage from './pages/MyBar/MyBarPage';
-import SpiritOverviewPage from './pages/SpiritOverview/SpiritOverviewPage';
-import SpiritSubtypesPage from './pages/SpiritSubtypes/SpiritSubtypesPage';
 
+// NEW IMPORT (make sure this path is correct based on your file structure)
+import OtherAlcoholPage from './pages/OtherAlcoholPage';
 
-import AlcoholTypeDetailPage from './pages/AlcoholType/[id]';
-import SubtypeDetailPage from './pages/Subtype/[id]';
-
-import SpiritProfilePage from './pages/Spirit/[id]';
-
-import AboutPage from './pages/About/AboutPage';
-import ContactPage from './pages/Contact/ContactPage';
-import SettingsPage from './pages/Settings/SettingsPage'; // <-- Make sure this import is here
-
-function App() {
+const App: React.FC = () => {
   return (
-    <UserPreferencesProvider>
+    <Router>
       <AuthProvider>
-        <SpiritsProvider>
+        <SpiritsProvider> {/* SpiritsContext is still used */}
           <RecommendationsProvider>
-            <Router>
-              <Toaster position="top-center" />
-              <Layout>
+            <div className="flex flex-col min-h-screen dark:bg-gray-900">
+              <Navbar />
+              <main className="flex-grow">
                 <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/explore" element={<SpiritListPage />} />
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/explore" element={<ExplorePage />} />
                   <Route path="/my-bar" element={<MyBarPage />} />
-                  <Route path="/alcohol-type/:id" element={<AlcoholTypeDetailPage />} />
-                  <Route path="/alcohol-type/:id/subtypes" element={<SpiritSubtypesPage />} />
-                  <Route path="/subtype/:id" element={<SubtypeDetailPage />} />
-
-                  <Route path="/spirit/:id" element={<SpiritProfilePage />} />
-
-                  <Route path="/category/:id" element={<SpiritOverviewPage />} />
                   <Route path="/about" element={<AboutPage />} />
                   <Route path="/contact" element={<ContactPage />} />
-                  <Route path="/settings" element={<SettingsPage />} /> {/* <--- THE FIX IS HERE! */}
+                  <Route path="/settings" element={<SettingsPage />} />
+                  <Route path="/signin" element={<SignInPage />} />
+                  <Route path="/signup" element={<SignUpPage />} />
+                  <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+                  {/* Spirit/Alcohol Detail Routes */}
+                  <Route path="/spirit/:id" element={<SpiritProfilePage />} />
+                  <Route path="/alcohol-type/:id" element={<SpiritOverviewPage />} />
+                  <Route path="/subtype/:id" element={<SubtypeDetailPage />} />
+
+                  {/* NEW ROUTE for Other Alcohol */}
+                  <Route path="/other-alcohol" element={<OtherAlcoholPage />} />
                 </Routes>
-              </Layout>
-            </Router>
+              </main>
+              <Footer />
+            </div>
           </RecommendationsProvider>
         </SpiritsProvider>
       </AuthProvider>
-    </UserPreferencesProvider>
+    </Router>
   );
-}
+};
 
 export default App;
